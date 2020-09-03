18b33e52272aef6e0e62cb7222aec6409c969528
MAALONSO <miguel.coba@gnp.com.mx>
2020-09-02 21:05:50 -0500

    Add comments

--- src/main/java/mockito/ValidNumber.java
+++ src/main/java/mockito/ValidNumber.java
@@ -1,19 +1,31 @@
 package mockito;
 
+/**
+ * Class ValidNumber.
+ */
 public class ValidNumber {
-	
+
+	/**
+	 * Consturctor por defecto.
+	 */
 	public ValidNumber() {
 		// Contructro por Defecto
 	}
-	
+
+	/**
+	 * Comprueba que el objeto se de tipo Integer.
+	 * 
+	 * @param o Object
+	 * @return boolean
+	 */
 	public boolean check(Object o) {
 		if (o instanceof Integer) {
 			if ((Integer) o < 10 && (Integer) o >= 0) {
 				return true;
-			}else {
+			} else {
 				return false;
 			}
-		}else {
+		} else {
 			return false;
 		}
 	}
